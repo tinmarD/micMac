09ff28ce453e723f156a4a3afc8aa5c5814634db
tinmarD <martin.deudon@protonmail.com>
2018-03-22 17:01:32 +0100

    modify docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{can:0,download:0,here:0,offlin:0,site:0,thi:0,use:0,welcom:0},titles:["micMac Manual Contents"],titleterms:{content:0,manual:0,micmac:0}})
